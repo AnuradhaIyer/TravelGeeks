d65dd2e200de99500dc86c769473ef4e27a4a7e8
AnuradhaIyer <tulsiraj14@gmail.com>
2018-05-17 22:38:37 -0700

    Adding Dev and Travel Portal

---
+++ TravelGeeks-Admin portal/README.md
@@ -0,0 +1,7 @@
+
+Spring Boot Application
+
+OKTA integration with Jira, Confluence, Bitbucket,Slack and Jenkins.
+
+
+![dev](https://user-images.githubusercontent.com/31361652/40217132-fe66ffc8-5a1f-11e8-9960-b9ae621a69bc.png)
---
+++ TravelGeeks-Admin portal/src/main/java/com/example/demo/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import static org.springframework.security.extensions.saml2.config.SAMLConfigurer.saml;
+
+@EnableWebSecurity
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Value("${security.saml2.metadata-url}")
+	String metadataUrl;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/saml/**").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.apply(saml())
+				.serviceProvider()
+					.keyStore()
+						.storeFilePath("")
+						.password("")
+						.keyname("")
+						.keyPassword("")
+						.and()
+					.protocol("https")
+					.hostname("")
+					.basePath("/")
+					.and()
+				.identityProvider()
+					.metadataFilePath(metadataUrl)
+					.and();
+	}
+}
